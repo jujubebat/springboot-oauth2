507bb177b49767edfa6473606141b6b755fe7c31
jujubebat <bbwwpark@naver.com>
2021-07-03 11:53:25 +0900

    feat: 네이버 oauth2 로그인 기능 구현

--- src/main/java/springboot/oauth2/controller/MemberOauth2Controller.java
+++ src/main/java/springboot/oauth2/controller/MemberOauth2Controller.java
@@ -10,6 +10,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -100,15 +101,97 @@ public class MemberOauth2Controller {
 
         StringBuilder result = new StringBuilder();
         result.append("<h1>카카오 로그인 성공</h1>");
-        result.append("이름 : " + nickname + "<br><br>");
-        result.append("성별 : " + gender+ "<br><br>");
-        result.append("생년월일 : " + birthday+ "<br><br>");
-        result.append("이메일 : " + email+ "<br><br>");
+        result.append("nickname : " + nickname + "<br><br>");
+        result.append("gender : " + gender + "<br><br>");
+        result.append("birthday : " + birthday + "<br><br>");
+        result.append("email : " + email + "<br><br>");
         result.append("token_type : " + token_type + "<br><br>");
         result.append("access_token : " + access_token + "<br><br>");
         result.append("access_token_expires_in : " + expires_in + "<br><br>");
         result.append("refresh_token : " + refresh_token + "<br><br>");
         result.append("refresh_token_expires_in : " + refresh_token_expires_in + "<br><br>");
+
+        return result.toString();
+    }
+
+    @GetMapping(value = "/naver")
+    @ResponseBody
+    public String naverOAuthRedirect(@RequestParam String code, @RequestParam String state) {
+        RestTemplate rt = new RestTemplate();
+
+        HttpHeaders accessTokenHeaders = new HttpHeaders();
+        accessTokenHeaders.add("Content-type", "application/x-www-form-urlencoded");
+
+        MultiValueMap<String, String> accessTokenParams = new LinkedMultiValueMap<>();
+        accessTokenParams.add("grant_type", "authorization_code");
+        accessTokenParams.add("client_id", "rhOOnuIVRpuKUyYMtzTh");
+        accessTokenParams.add("client_secret", "SbYD_Wy7lz");
+        accessTokenParams.add("code", code);
+        accessTokenParams.add("state", state);
+
+        HttpEntity<MultiValueMap<String, String>> accessTokenRequest = new HttpEntity<>(
+            accessTokenParams, accessTokenHeaders);
+
+        ResponseEntity<String> accessTokenResponse = rt.exchange(
+            "https://nid.naver.com/oauth2.0/token",
+            HttpMethod.POST,
+            accessTokenRequest,
+            String.class
+        );
+
+        String jsonString = accessTokenResponse.getBody();
+        JSONObject jsonObject = new JSONObject(jsonString);
+        String access_token = jsonObject.getString("access_token");
+        String refresh_token = jsonObject.getString("refresh_token");
+        String token_type = jsonObject.getString("token_type");
+        String expires_in = jsonObject.getString("expires_in");
+
+        // profile api로 생성해둔 헤더를 담아서 요청을 보냅니다.
+        HttpHeaders profileRequestHeader = new HttpHeaders();
+        profileRequestHeader.add("Authorization", "Bearer " + access_token);
+
+        HttpEntity<HttpHeaders> profileHttpEntity = new HttpEntity<>(profileRequestHeader);
+
+        // profile api로 생성해둔 헤더를 담아서 요청을 보냅니다.
+        ResponseEntity<String> profileResponse = rt.exchange(
+            "https://openapi.naver.com/v1/nid/me",
+            HttpMethod.POST,
+            profileHttpEntity,
+            String.class
+        );
+
+        String jsonString2 = profileResponse.getBody();
+        JSONObject jsonObject2 = new JSONObject(jsonString2);
+        JSONObject response = (JSONObject) jsonObject2.get("response");
+        String id = response.getString("id");
+        String nickname = response.getString("nickname");
+        String profile_image = response.getString("profile_image");
+        String age = response.getString("age");
+        String gender = response.getString("gender");
+        String email = response.getString("email");
+        String mobile = response.getString("mobile");
+        String mobile_e164 = response.getString("mobile_e164");
+        String name = response.getString("name");
+        String birthday = response.getString("birthday");
+        String birthyear = response.getString("birthyear");
+
+        StringBuilder result = new StringBuilder();
+        result.append("<h1>네이버 로그인 성공</h1>");
+        result.append("id : " + id + "<br><br>");
+        result.append("nickname : " + nickname + "<br><br>");
+        result.append("profile_image : " + profile_image + "<br><br>");
+        result.append("age : " + age + "<br><br>");
+        result.append("gender : " + gender + "<br><br>");
+        result.append("email : " + email + "<br><br>");
+        result.append("mobile : " + mobile + "<br><br>");
+        result.append("mobile_e164 : " + mobile_e164 + "<br><br>");
+        result.append("name : " + name + "<br><br>");
+        result.append("birthday : " + birthday + "<br><br>");
+        result.append("birthyear : " + birthyear + "<br><br>");
+        result.append("token_type : " + token_type + "<br><br>");
+        result.append("access_token : " + access_token + "<br><br>");
+        result.append("access_token_expires_in : " + expires_in + "<br><br>");
+        result.append("refresh_token : " + refresh_token + "<br><br>");
 
         return result.toString();
     }
